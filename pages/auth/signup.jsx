0c2f3ae31d451c6fea93d2e057ad5c259039ede1
EdgarMTdev <edgarmontiel961@gmail.com>
2022-05-10 08:58:15 -0500

    signup design

--- pages/auth/signup.jsx
+++ pages/auth/signup.jsx
@@ -12,9 +12,14 @@ export default function signup() {
   return (
     <div className=' w-4/6 h-[500px] mx-auto mt-5 bg-slate-100 shadow-xl flex'>
       <section className=' w-[130%] background'></section>
-      <form onSubmit={() => {}} className=' w-4/5 py-20 px-12'>
+      <form onSubmit={() => {}} className=' w-4/5 py-16 px-12'>
         {loading && <ImSpinner2 className=' animate-spin ml-auto w-5 h-5 m-3' />}
         <h1 className=' text-center text-2xl font-semibold mb-12'>Sign Up</h1>
+        <InputForm
+          type={'text'}
+          name={'name'}
+          placeholder={'Name'}
+        />
         <InputForm
           type={'text'}
           name={'email'}
@@ -25,7 +30,7 @@ export default function signup() {
           name={'password'}
           placeholder={'Password'}
         />
-        <Button title={'SIGN IN'} />
+        <Button title={'REGISTER'} />
       </form>
     </div>
   )
