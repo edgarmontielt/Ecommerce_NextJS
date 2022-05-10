5d58b42deae620bdfa16a8522eb6f55426fd6bf2
EdgarMTdev <edgarmontiel961@gmail.com>
2022-05-10 00:33:10 -0500

    add firebase auth

--- config/firebase.js
+++ config/firebase.js
@@ -1,10 +1,11 @@
 import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-     apiKey: process.env.API_KEY,
-     authDomain: process.env.AUTH_DOMAIN,
-     projectId: process.env.PROJECT_ID,
+     apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
+     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
      storageBucket: process.env.STORAGE_BUCKET,
      messagingSenderId: process.env.MESSAGING_SENDER_ID,
      appId: process.env.APP_ID,
@@ -14,3 +15,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 
 export const database = getFirestore(app)
+export const auth = getAuth(app)
--- pages/auth/login.jsx
+++ pages/auth/login.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import Button from '../../components/auth/Button'
 import InputForm from '../../components/auth/InputForm'
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../config/firebase';
 
 export default function login() {
+
+    const login = (event) => {
+        event.preventDefault()
+        const { email: { value: email }, password: { value: password } } = event.target
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                const user = userCredential.user
+                console.log(user);
+            }).catch((error) => {
+                const errorMessage = error.message;
+                console.log(errorMessage);
+            });
+    }
     return (
         <div className=' w-4/6 h-[500px] mx-auto mt-12 bg-slate-100 shadow-xl flex'>
             <section className=' w-[130%] background'></section>
-            <form onSubmit={() => { }} className=' w-4/5 py-20 px-12'>
+            <form onSubmit={login} className=' w-4/5 py-20 px-12'>
                 <h1 className=' text-center text-2xl font-semibold mb-14'>Login</h1>
                 <InputForm
                     type={'text'}
@@ -18,7 +33,7 @@ export default function login() {
                     name={'password'}
                     placeholder={'Password'}
                 />
-                <Button title={'SIGN IN'}/>
+                <Button title={'SIGN IN'} />
             </form>
         </div>
     )
