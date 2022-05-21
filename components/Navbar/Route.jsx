5c8854739e0404e92af8e0c355dc5f8a4c965530
Edgar Montiel <61670650+edgarmontielt@users.noreply.github.com>
2022-05-20 21:56:59 -0500

    Delete components/Navbar directory

--- components/Navbar/Route.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Link from 'next/link'
-
-export default function Route({ to, title }) {
-    return (
-        <li className='hover:underline'><Link href={to}>{title}</Link></li>
-    )
-}
